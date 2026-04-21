6c09f175f38be5821b53ac0e8daecf6113214394
satyam singh <satyamsinghh830@gmail.com>
2026-04-22 00:17:26 +0530

    url change to vercel urls

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -26,7 +26,7 @@ app.use(express.json());
 
 app.use(cors(
    {
-     origin :  "http://localhost:5173",
+     origin :  "https://restaurant-app-one-dusky.vercel.app",
     credentials : true
    }
 ));
